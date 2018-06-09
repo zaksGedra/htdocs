e3552554ff5fa2882857267851a396065961a678
Žaks <Zaks.Gedra@gmail.com>
2018-06-09 11:47:38 +0300

    save changes

--- diena5.php
+++ diena5.php
@@ -1,4 +1,68 @@
 <meta http-equiv="refresh" content="5; URL=localhost:8888">
+<?php
+
+
+class Device{
+
+	public $year;
+	public $manufacturer;
+	public static $all_manufacturers;
+
+	function __construct(){
+		print("Parent Construction!");
+	}
+	
+	public function getYear(){
+		return $this->year;
+	}
+	public function setYear($year){
+		$this->year = $year;
+	}
+
+	public function getManufacturer(){
+		return $this->company;
+	}
+	public function setManufacturer($company){
+		$this->company = $company;
+	}
+
+	public function getFullInfo(){
+		return ($this->company . " " . $this->year);
+	}
+
+	public static function getAllManufacturers(){
+		
+		//db?
+
+		return ["Siemens", "Bosch", "Apple", "Audi"];
+		// return array("Siemens", "Bosch", "Apple", "Audi");
+	}
+
+}
+
+class Microphone extends Device{
+	function __construct(){
+		parent::__construct();
+		print("Child Construction!");
+		parent::__construct();
+	}
+
+	public function getFullInfo(){
+		return ($this->company . " " . $this->year . " ( mikrofons! ) ");
+	}
+}
+
+
+// $device = new Device();
+// $device->setYear(2017);
+// $device->setManufacturer("Siemens");
+// print($device->getFullInfo());
+// var_dump(Device::getAllManufacturers());
+$mikrofons = new Microphone();
+$mikrofons->setYear(2016);
+$mikrofons->setManufacturer("Samsung");
+print($mikrofons->getFullInfo());
+
 
 
 
